5356ebfbf62ec69bfa3fec817acadfc9cb5edcb5
shadow-oss <55443830+shadow-oss@users.noreply.github.com>
2021-07-20 07:47:46 +0500

    commit

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ var cash=document.querySelector("#cash");
 var btn1=document.querySelector("#btn1");
 var error=document.querySelector(".error");
 var error2=document.querySelector(".error2");
-var tabl=document.querySelector("#tab");
+var error3=document.querySelector(".error3");
+var tabl=document.querySelector("#output");
+var amt=0;
+var amtC=[2000,500,100,20,10,5,1];
 getCashGiven=function(){
     console.log("enter");
     if(txt1.value>0){
         console.log("enter1");
         btn1.style.display="none";
+        error.style.display="none";
         cash.style.display="block";
         console.log("Done");
     }
@@ -18,15 +22,46 @@ getCashGiven=function(){
         error.style.display="block";
     }
 }
- getChange=function(){
-    var amt=txt1.value-txt2.value;
+ getChange=()=>{
+     for(var i=0;i<amtC.length;i++){
+        document.querySelector("#c"+amtC[i]).innerHTML=0
+     }
+     
+     amt=txt2.value-txt1.value;
     if(!amt){
         error2.style.display="block";
+        error3.style.display="none";
+        tabl.style.display="none";
+    }
+    else if((txt2.value-txt1.value)<0){
+        error2.style.display="none";
+        tabl.style.display="none";
+        error3.style.display="block";
     }
     else{
         error2.style.display="none";
+        error3.style.display="none";
+        for(var x=0;x<amtC.length;x++){
+           amt=calc(amtC[x],amt);
+            if(amt==0){
+                break
+            }
+        }
         tabl.style.display="block";
-
-
     }
+}
+
+
+function calc(cash,amt1){
+    var cal=amt1/cash;
+    if(cal>0){
+       cal= Math.floor(cal);
+        amt=amt1-(cash*cal);
+        document.querySelector("#c"+cash).innerHTML=cal;
+        return amt;
+    }
+    else{
+        return amt;
+    }
+
 }
